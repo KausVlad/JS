a05e5286e54b103e8a3ebf285fb9824b7dbb9c1c
KausVlad <kausvirtual@gmail.com>
2023-03-01 22:33:58 +0200

    Logic and Press F to pay respect!!

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 'use strict';
-let firstNumber;
-let secondNumber;
+let firstNumber, secondNumber;
 
 firstNumber = prompt('Enter first number');
 secondNumber = prompt('Enter second number');
+console.log(firstNumber, secondNumber);
 
-let sum = Number(firstNumber) + Number(secondNumber);
-let subtract = Number(firstNumber) - Number(secondNumber);
-let multiple = Number(firstNumber) * Number(secondNumber);
-let divide = Number(firstNumber) / Number(secondNumber);
-alert(
-  `sum: ${sum}, subtract: ${subtract}, multiple: ${multiple}, divide: ${divide}`
-);
+if ((firstNumber && secondNumber) !== '') {
+  alert(`sum: ${Number(firstNumber) + Number(secondNumber)}`);
+
+  if (Number(firstNumber) < Number(secondNumber)) {
+    if (confirm('are you sure about that?')) {
+      alert(Number(firstNumber) - Number(secondNumber));
+    }
+  }
+
+  alert(
+    `the result of multiplying: ${Number(firstNumber) * Number(secondNumber)}`
+  );
+  if (Number(secondNumber) === 0) {
+    alert('second number must be not a 0');
+  } else {
+    alert(
+      `the result of dividing: ${Number(firstNumber) / Number(secondNumber)}`
+    );
+  }
+} else {
+  alert(`I don't see 2 values, BB and GG!! Press F to pay respect!!`);
+}
