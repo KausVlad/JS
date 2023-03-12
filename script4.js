81b3b970dda41c81b70caa1380584cc9bc6edc04
KausVlad <kausvirtual@gmail.com>
2023-03-12 22:10:16 +0200

    refactoring

--- script4.js
+++ script4.js
@@ -11,11 +11,16 @@ const user = {
 };
 
 let bigBrainCalc = {
-  firstNumber: undefined,
-  secondNumber: undefined,
+  firstNumber: null,
+  secondNumber: null,
+  newFirstNumber: null,
+  newSecondNumber: null,
+  errorMassage: `It is possible that one or two values are not a number, BB and GG!! Press F to pay respect!!`,
   ask() {
     this.firstNumber = prompt('Enter first number');
     this.secondNumber = prompt('Enter second number');
+    this.newFirstNumber = +this.firstNumber;
+    this.newSecondNumber = +this.secondNumber;
   },
   numberChecking() {
     return this.firstNumber === '' ||
@@ -28,45 +33,43 @@ let bigBrainCalc = {
   sum() {
     return this.numberChecking()
       ? `sum ${this.firstNumber} + ${this.secondNumber} = ${
-          Number(this.firstNumber) + Number(this.secondNumber)
+          this.newFirstNumber + this.newSecondNumber
         }`
-      : `It is possible that one or two values are not a number, BB and GG!! Press F to pay respect!!`;
+      : this.errorMassage;
   },
   mul(x, y) {
     return this.numberChecking()
       ? `the result of multiplying ${this.firstNumber} * ${
           this.secondNumber
-        } = ${Number(this.firstNumber) * Number(this.secondNumber)}`
-      : `It is possible that one or two values are not a number, BB and GG!! Press F to pay respect!!`;
+        } = ${this.newFirstNumber * this.newSecondNumber}`
+      : this.errorMassage;
   },
   //bonus
   subtract() {
     if (this.numberChecking()) {
-      if (Number(this.firstNumber) < Number(this.secondNumber)) {
+      if (this.newFirstNumber < this.newSecondNumber) {
         return confirm('are you sure about that?')
           ? `result of subtraction ${this.firstNumber} - ${
               this.secondNumber
-            } = ${
-              Number(this.firstNumber) - Number(this.secondNumber)
-            }, you are tuff guy`
+            } = ${this.newFirstNumber - this.newSecondNumber}, you are tuff guy`
           : 'the result will be less than 0 and not acceptable to you';
       } else {
         return `result of subtraction ${this.firstNumber} - ${
           this.secondNumber
-        } = ${Number(this.firstNumber) - Number(this.secondNumber)}`;
+        } = ${this.newFirstNumber - this.newSecondNumber}`;
       }
     }
-    return `It is possible that one or two values are not a number, BB and GG!! Press F to pay respect!!`;
+    return this.errorMassage;
   },
   divide() {
     if (this.numberChecking()) {
-      return Number(this.secondNumber) === 0
+      return this.newSecondNumber === 0
         ? 'cannot be divided by zero'
         : `the result of dividing ${this.firstNumber} / ${
             this.secondNumber
-          } = ${Number(this.firstNumber) / Number(this.secondNumber)}`;
+          } = ${this.newFirstNumber / this.newSecondNumber}`;
     }
-    return `It is possible that one or two values are not a number, BB and GG!! Press F to pay respect!!`;
+    return this.errorMassage;
   },
   ///extra bonus
   mathSelector() {
@@ -75,15 +78,14 @@ let bigBrainCalc = {
 
     if (operationType === '+') {
       alert(this.sum());
-    }
-    if (operationType === '-') {
+    } else if (operationType === '-') {
       alert(this.subtract());
-    }
-    if (operationType === '/') {
+    } else if (operationType === '/') {
       alert(this.mul());
-    }
-    if (operationType === '*') {
+    } else if (operationType === '*') {
       alert(this.divide());
+    } else {
+      alert('invalid operation');
     }
   },
 };
